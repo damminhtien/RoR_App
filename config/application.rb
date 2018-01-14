0bdeddec7274685ebf22aa825f9bb24b287f3de0
DamMinhTien <damminhtienchl@gmail.com>
2018-01-14 16:22:37 +0700

    static page,test and i18n

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RorApp
+module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
@@ -14,5 +14,14 @@ module RorApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Where the I18n library should search for translation files
+    I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+
+    # Whitelist locales available for the application
+    I18n.available_locales = [:en, :vi]
+
+    # Set default locale to something other than :en
+    I18n.default_locale = :en
   end
 end
